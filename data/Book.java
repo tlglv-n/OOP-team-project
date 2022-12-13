98f9e8b0d21ec7cda22f31f6336d91e11e73b9e6
tlglv <98637977+kastel559@users.noreply.github.com>
2022-12-14 00:12:03 +0600

    Revert "check"
    This reverts commit 3b300f0480e8ae36f44c1dc3c0662e3a40946208, reversing
    changes made to 536e63f8415467baef1bdf9604e5d8f2b7189779.

--- data/Book.java
+++ data/Book.java
@@ -9,6 +9,7 @@ public class Book
 {
 	
 	private String name;	
+	private int id;
 	private Date deadline;
 
 	public Book(){}
@@ -16,22 +17,31 @@ public class Book
 	public String getName(){
 		return name;
 	}
-	
-	public Date getDeadline(){
+	public int id() {
+		return id;
+	}
+	public Date getDeadline() {
 		return deadline;
 	}
+	
 	public boolean equals(Object o) {
 		if(o == null) {return false;}
 		if(this == o) {return true;}
 		if(this.getClass() != o.getClass()) {return false;}
 		Book b = (Book) o;
-		return this.name.equals(b.name) && this.deadline.equals(b.deadline);
+		return id == b.id;
 	}
 	public int hashCode() {
-		return Objects.hash(name, deadline);	
+		return Objects.hash(id);	
 	}
 	public String toString() {
-		return "Book: " + name;
+		return "Book: " + name + ", ID: " + id;
+	}
+	
+	public int compareTo(Book b) {
+		if(id > b.id) {return 1;}
+		if(id < b.id) {return -1;}
+		return 0;
 	}
 }
 
