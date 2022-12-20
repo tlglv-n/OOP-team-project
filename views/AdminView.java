11258d43b69fbc1d74a2271dc36314faeca7e9d2
ChingisB <c_bogdatov@kbtu.kz>
2022-12-20 12:42:40 +0600

    upd

--- views/AdminView.java
+++ views/AdminView.java
@@ -1,30 +1,78 @@
 package views ;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import data.Data;
 import users.Admin;
+import users.UserType;
+import users.UserFactory;
 
 public class AdminView extends UserView{
-
-	private Admin admin;
 
 	protected AdminView(){}
 
 	public AdminView(Admin admin){
-		this.admin = admin;
+		super(admin);
 	}
 
-	public Admin getAdmin(){
-		return admin;
+	public void createUser() throws IOException{
+		UserType type;
+		String password;
+		String login = "Default";
+		writer.write("Choose kind of user you want to create: \n");
+		for(UserType ut: UserType.values()){
+			writer.write(ut.toString() + '\n');
+		}
+		while(true){
+			try {
+				type = UserType.valueOf(reader.readLine());
+				break;
+			}
+			catch (NoSuchElementException nsee){
+				writer.write("No such option \n", 0, 0);
+			}
+		}
+		while(true){
+			login = reader.readLine();
+			final String costyl = login;
+			if(Data.getInstance().getUsers().stream()
+			.filter(u -> costyl.equals(u.getLogin()))
+			.collect(Collectors.toList()).isEmpty()){
+				break;
+			}
+			writer.write("Username already used, try again\n");
+		}
+		writer.write("Insert password: \n");
+		password = reader.readLine();
+		UserFactory.getUser(login, password, type);
 	}
 
 	public void main(){
-		reader = new BufferedReader(new InputStreamReader(System.in));
-		writer = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+		while(true){
+			try{
+				writer.write("0. Exit\n");
+				writer.write("1. View news\n");
+				writer.write("2. View personal info\n");
+				writer.write("3. Change password\n");
+				writer.write("4. Create user\n");
+				String ans = reader.readLine();
+				switch(ans){
+					case "0":
+						return;
+					case "1":
+						viewNews();
+					case "2":
+						viewPersonalInfo();
+					case "3":
+						changePassword();
+					case "4":
+						createUser();
+				}
+			}
+			catch (IOException ioe){
+			}
+		}
 	}
-
 }
 
